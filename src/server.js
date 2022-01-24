5c71225e87f4531e1f42d7457e03cca9367faaaa
Asmaa <Asmaa99musse@gmail.com>
2022-01-24 19:14:02 +0000

    added schema db

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-require("dotenv").config();
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const req = require("express/lib/request");
-// const sequelizeStore = require("connect-session-sequelize")(session.Store);
 const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const path = require("path");
 
 const routes = require("./routes");
 const connection = require("./config/connection");
+const helpers = require("./helpers/handlerbars");
 
 const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
@@ -24,8 +26,9 @@ const sessionOptions = {
   }),
 };
 
-const hbs = expressHandlebars.create({});
-const app = express();
+const hbs = expressHandlebars.create({
+  helpers: helpers,
+});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -33,7 +36,7 @@ app.set("view engine", "handlebars");
 app.use(session(sessionOptions));
 app.use("express.json()");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(routes);
 
 const init = async () => {
